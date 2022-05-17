eb5ae5902d4048c14f22a97e3b81010ee4f543b9
Xelcius <97951640+RimuruDemonlord@users.noreply.github.com>
2022-05-17 08:32:14 -0700

    Update aifunctions.py

--- FallenRobot/modules/aifunctions.py
+++ FallenRobot/modules/aifunctions.py
@@ -29,7 +29,7 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "I am fairly found and I was made by @anonymous_was_bot."
+   pro = "I am fairly young and I was made by @xelcius."
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
@@ -37,7 +37,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Fallen"
+   pro = "My name is Tessia"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
